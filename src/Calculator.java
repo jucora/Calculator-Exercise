0d8994245f4a18201e71c3be28b9f995a32cee8d
jucora <artjulius@hotmail.com>
2021-01-20 22:01:01 -0500

    Addition and Subtraction Methods Added

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+	private double num1, num2;
+	public Calculator(double num1, double num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public double addition() {
+		return this.num1 + this.num2;
+	}
+	
+	public double subtraction() {
+		return this.num1 - this.num2;
+	}
+}
